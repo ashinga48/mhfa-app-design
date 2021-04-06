4ddd031a79f22dfcb39c846804e6bb72a7730159
Ravi Kesanam <kesanam.ravi@gmail.com>
2021-04-07 09:32:37 +1000

    added screens and prototype

---
+++ proto/viewer/story.js
@@ -0,0 +1,778 @@
+var story = {
+ "docName": "mhfa",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "mhfa",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "58E89F18-5168-4C1C-9752-0E9BF850C6E1",
+   "groupID": "B0DF64F9-B847-4BC3-B7DB-6ED879B71F83",
+   "index": 0,
+   "image": "iphone_12_pro_copy_15.png",
+   "image2x": "iphone_12_pro_copy_15@2x.png",
+   "width": 414,
+   "height": 896,
+   "x": -3799,
+   "y": -1022.9999999999999,
+   "title": "iPhone 12 Pro Copy 15",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 0,
+      "y": 0,
+      "width": 414,
+      "height": 896
+     },
+     "isParentFixed": false,
+     "page": 9,
+     "index": 0
+    }
+   ]
+  },
+  {
+   "id": "7CD52A67-169E-46C2-9DC0-47BD84E80F04",
+   "groupID": "B0DF64F9-B847-4BC3-B7DB-6ED879B71F83",
+   "index": 1,
+   "image": "iphone_12_pro_copy_13.png",
+   "image2x": "iphone_12_pro_copy_13@2x.png",
+   "width": 414,
+   "height": 896,
+   "x": -3406,
+   "y": 262,
+   "title": "iPhone 12 Pro Copy 13",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 0,
+      "y": 0,
+      "width": 414,
+      "height": 896
+     },
+     "isParentFixed": false,
+     "page": 5,
+     "index": 0
+    }
+   ]
+  },
+  {
+   "id": "F3323766-AFE5-489C-9F2F-36788C00BCFD",
+   "groupID": "B0DF64F9-B847-4BC3-B7DB-6ED879B71F83",
+   "index": 2,
+   "image": "iphone_12_pro_copy_3.png",
+   "image2x": "iphone_12_pro_copy_3@2x.png",
+   "width": 414,
+   "height": 896,
+   "x": -3406,
+   "y": 1727,
+   "title": "iPhone 12 Pro Copy 3",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "15E791FE-96D7-471D-8CAA-B659F3A38B22",
+   "groupID": "B0DF64F9-B847-4BC3-B7DB-6ED879B71F83",
+   "index": 3,
+   "image": "iphone_12_pro_copy_16.png",
+   "image2x": "iphone_12_pro_copy_16@2x.png",
+   "width": 414,
+   "height": 896,
+   "x": -3232,
+   "y": -1022.9999999999999,
+   "title": "iPhone 12 Pro Copy 16",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 126,
+      "y": 280,
+      "width": 164,
+      "height": 44
+     },
+     "isParentFixed": false,
+     "page": 0,
+     "index": 0
+    }
+   ]
+  },
+  {
+   "id": "25ADE4F3-3D1A-4B8A-8649-68A47E73842B",
+   "groupID": "B0DF64F9-B847-4BC3-B7DB-6ED879B71F83",
+   "index": 4,
+   "image": "iphone_12_pro_copy_12.png",
+   "image2x": "iphone_12_pro_copy_12@2x.png",
+   "width": 414,
+   "height": 896,
+   "x": -2892,
+   "y": 1727,
+   "title": "iPhone 12 Pro Copy 12",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 0,
+      "y": 0,
+      "width": 414,
+      "height": 896
+     },
+     "isParentFixed": false,
+     "page": 9,
+     "index": 0
+    }
+   ]
+  },
+  {
+   "id": "3ED70487-6F14-431C-9A96-FD4028A5B43E",
+   "groupID": "B0DF64F9-B847-4BC3-B7DB-6ED879B71F83",
+   "index": 5,
+   "image": "iphone_12_pro.png",
+   "image2x": "iphone_12_pro@2x.png",
+   "width": 414,
+   "height": 896,
+   "x": -2592,
+   "y": 272,
+   "title": "iPhone 12 Pro",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 115,
+      "y": 279,
+      "width": 185,
+      "height": 185
+     },
+     "isParentFixed": false,
+     "page": 3,
+     "index": 0
+    },
+    {
+     "rect": {
+      "x": 79,
+      "y": 464,
+      "width": 256,
+      "height": 107
+     },
+     "isParentFixed": false,
+     "page": 1,
+     "index": 1
+    }
+   ]
+  },
+  {
+   "id": "1BA5F34A-9956-4B6B-9033-A5C7575181A0",
+   "groupID": "B0DF64F9-B847-4BC3-B7DB-6ED879B71F83",
+   "index": 6,
+   "image": "iphone_12_pro_copy_19.png",
+   "image2x": "iphone_12_pro_copy_19@2x.png",
+   "width": 414,
+   "height": 896,
+   "x": -2378,
+   "y": 1727,
+   "title": "iPhone 12 Pro Copy 19",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 0,
+      "y": 0,
+      "width": 414,
+      "height": 896
+     },
+     "isParentFixed": false,
+     "page": 4,
+     "index": 0
+    }
+   ]
+  },
+  {
+   "id": "3DCFCB4A-CA86-4816-9A0B-334F101444C7",
+   "groupID": "B0DF64F9-B847-4BC3-B7DB-6ED879B71F83",
+   "index": 7,
+   "image": "iphone_12_pro_copy_18.png",
+   "image2x": "iphone_12_pro_copy_18@2x.png",
+   "width": 414,
+   "height": 896,
+   "x": -1864,
+   "y": 1727,
+   "title": "iPhone 12 Pro Copy 18",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 31,
+      "y": 280,
+      "width": 354,
+      "height": 45
+     },
+     "isParentFixed": false,
+     "page": 6,
+     "index": 0
+    }
+   ]
+  },
+  {
+   "id": "81294F65-A87D-4E08-AA0C-86A0A1BA5DB0",
+   "groupID": "B0DF64F9-B847-4BC3-B7DB-6ED879B71F83",
+   "index": 8,
+   "image": "iphone_12_pro_copy_2.png",
+   "image2x": "iphone_12_pro_copy_2@2x.png",
+   "width": 414,
+   "height": 896,
+   "x": -1166,
+   "y": 2623,
+   "title": "iPhone 12 Pro Copy 2",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 0,
+      "y": 825,
+      "width": 147,
+      "height": 73
+     },
+     "isParentFixed": false,
+     "page": 9,
+     "index": 0
+    }
+   ]
+  },
+  {
+   "id": "67D91011-D070-44F5-91CA-3C934F0ED3A0",
+   "groupID": "B0DF64F9-B847-4BC3-B7DB-6ED879B71F83",
+   "index": 9,
+   "image": "iphone_12_pro_copy_4.png",
+   "image2x": "iphone_12_pro_copy_4@2x.png",
+   "width": 414,
+   "height": 896,
+   "x": -882.9999999999999,
+   "y": 190,
+   "title": "iPhone 12 Pro Copy 4",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 363,
+      "y": 852,
+      "width": 23,
+      "height": 24
+     },
+     "isParentFixed": false,
+     "page": 11,
+     "index": 0
+    },
+    {
+     "rect": {
+      "x": 44,
+      "y": 808,
+      "width": 151,
+      "height": 12
+     },
+     "isParentFixed": false,
+     "page": 8,
+     "index": 1
+    },
+    {
+     "rect": {
+      "x": 172,
+      "y": 856,
+      "width": 58,
+      "height": 17
+     },
+     "isParentFixed": false,
+     "page": 10,
+     "index": 2
+    },
+    {
+     "rect": {
+      "x": 286,
+      "y": 852,
+      "width": 23,
+      "height": 23
+     },
+     "isParentFixed": false,
+     "page": 7,
+     "index": 3
+    },
+    {
+     "rect": {
+      "x": 21,
+      "y": 157,
+      "width": 377,
+      "height": 137
+     },
+     "isParentFixed": false,
+     "page": 12,
+     "index": 4
+    },
+    {
+     "rect": {
+      "x": 100,
+      "y": 28,
+      "width": 216,
+      "height": 64
+     },
+     "isParentFixed": false,
+     "page": 5,
+     "index": 5
+    }
+   ]
+  },
+  {
+   "id": "D2323D8D-F015-4CB3-B6A1-7FCD39E3634A",
+   "groupID": "B0DF64F9-B847-4BC3-B7DB-6ED879B71F83",
+   "index": 10,
+   "image": "iphone_12_pro_copy_6.png",
+   "image2x": "iphone_12_pro_copy_6@2x.png",
+   "width": 414,
+   "height": 896,
+   "x": -226.00000000000003,
+   "y": 1302,
+   "title": "iPhone 12 Pro Copy 6",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 16,
+      "y": 147,
+      "width": 380,
+      "height": 114
+     },
+     "isParentFixed": false,
+     "page": 15,
+     "index": 0
+    },
+    {
+     "rect": {
+      "x": 253,
+      "y": 78,
+      "width": 135,
+      "height": 44
+     },
+     "isParentFixed": false,
+     "page": 13,
+     "index": 1
+    },
+    {
+     "rect": {
+      "x": 16.5,
+      "y": 78,
+      "width": 204,
+      "height": 44
+     },
+     "isParentFixed": false,
+     "page": 14,
+     "index": 2
+    },
+    {
+     "rect": {
+      "x": 43,
+      "y": 856,
+      "width": 51,
+      "height": 17
+     },
+     "isParentFixed": false,
+     "page": 9,
+     "index": 3
+    }
+   ]
+  },
+  {
+   "id": "BA8F82FE-EA82-4379-9EF8-E0C4623C9D2C",
+   "groupID": "B0DF64F9-B847-4BC3-B7DB-6ED879B71F83",
+   "index": 11,
+   "image": "iphone_12_pro_copy_5.png",
+   "image2x": "iphone_12_pro_copy_5@2x.png",
+   "width": 414,
+   "height": 896,
+   "x": -19.000000000000114,
+   "y": -1408,
+   "title": "iPhone 12 Pro Copy 5",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 0,
+      "y": 821,
+      "width": 144,
+      "height": 79
+     },
+     "isParentFixed": false,
+     "page": 9,
+     "index": 0
+    },
+    {
+     "rect": {
+      "x": 17,
+      "y": 145,
+      "width": 382,
+      "height": 79
+     },
+     "isParentFixed": false,
+     "page": 12,
+     "index": 1
+    }
+   ]
+  },
+  {
+   "id": "27FD8385-70D6-40BB-8AB2-1612F8AC9403",
+   "groupID": "B0DF64F9-B847-4BC3-B7DB-6ED879B71F83",
+   "index": 12,
+   "image": "iphone_12_pro_copy_9.png",
+   "image2x": "iphone_12_pro_copy_9@2x.png",
+   "width": 414,
+   "height": 896,
+   "x": 483,
+   "y": -1431,
+   "title": "iPhone 12 Pro Copy 9",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 0,
+      "y": 0,
+      "width": 414,
+      "height": 896
+     },
+     "isParentFixed": false,
+     "page": 11,
+     "index": 0
+    }
+   ]
+  },
+  {
+   "id": "7790EA99-351A-4CFB-BD0D-1A6E22EA8BB2",
+   "groupID": "B0DF64F9-B847-4BC3-B7DB-6ED879B71F83",
+   "index": 13,
+   "image": "iphone_12_pro_copy_8.png",
+   "image2x": "iphone_12_pro_copy_8@2x.png",
+   "width": 414,
+   "height": 896,
+   "x": 1017,
+   "y": 802,
+   "title": "iPhone 12 Pro Copy 8",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 13,
+      "y": 15,
+      "width": 54,
+      "height": 54
+     },
+     "isParentFixed": false,
+     "page": 10,
+     "index": 0
+    }
+   ]
+  },
+  {
+   "id": "B7B7E7A0-54FE-455F-BC27-202B5E71465F",
+   "groupID": "B0DF64F9-B847-4BC3-B7DB-6ED879B71F83",
+   "index": 14,
+   "image": "iphone_12_pro_copy_7.png",
+   "image2x": "iphone_12_pro_copy_7@2x.png",
+   "width": 414,
+   "height": 896,
+   "x": 1017,
+   "y": 1900,
+   "title": "iPhone 12 Pro Copy 7",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 125,
+      "y": 593,
+      "width": 213,
+      "height": 45
+     },
+     "isParentFixed": false,
+     "page": 10,
+     "index": 0
+    }
+   ]
+  },
+  {
+   "id": "D789A04F-D0E1-493D-9605-3331F991A0C6",
+   "groupID": "B0DF64F9-B847-4BC3-B7DB-6ED879B71F83",
+   "index": 15,
+   "image": "iphone_12_pro_copy_21.png",
+   "image2x": "iphone_12_pro_copy_21@2x.png",
+   "width": 414,
+   "height": 896,
+   "x": 1551,
+   "y": 1308,
+   "title": "iPhone 12 Pro Copy 21",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 24,
+      "y": 520,
+      "width": 366,
+      "height": 52
+     },
+     "isParentFixed": false,
+     "page": 17,
+     "index": 0
+    },
+    {
+     "rect": {
+      "x": 236,
+      "y": 455,
+      "width": 133,
+      "height": 13
+     },
+     "isParentFixed": false,
+     "page": 16,
+     "index": 1
+    },
+    {
+     "rect": {
+      "x": 43,
+      "y": 856,
+      "width": 51,
+      "height": 17
+     },
+     "isParentFixed": false,
+     "page": 9,
+     "index": 2
+    }
+   ]
+  },
+  {
+   "id": "C8B6A5EF-EBD0-4F69-9ACC-A026C973C150",
+   "groupID": "B0DF64F9-B847-4BC3-B7DB-6ED879B71F83",
+   "index": 16,
+   "image": "iphone_12_pro_copy_22.png",
+   "image2x": "iphone_12_pro_copy_22@2x.png",
+   "width": 414,
+   "height": 896,
+   "x": 2158,
+   "y": 1843,
+   "title": "iPhone 12 Pro Copy 22",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 53,
+      "y": 455,
+      "width": 120,
+      "height": 13
+     },
+     "isParentFixed": false,
+     "page": 15,
+     "index": 0
+    }
+   ]
+  },
+  {
+   "id": "1F82E2B4-ECA3-4690-A05D-161E03EB54BA",
+   "groupID": "B0DF64F9-B847-4BC3-B7DB-6ED879B71F83",
+   "index": 17,
+   "image": "iphone_12_pro_copy_23.png",
+   "image2x": "iphone_12_pro_copy_23@2x.png",
+   "width": 414,
+   "height": 896,
+   "x": 2179,
+   "y": 802,
+   "title": "iPhone 12 Pro Copy 23",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 7,
+      "y": 2,
+      "width": 59,
+      "height": 48
+     },
+     "isParentFixed": false,
+     "page": 15,
+     "index": 0
+    }
+   ]
+  }
+ ],
+ "groups": [
+  {
+   "id": "B0DF64F9-B847-4BC3-B7DB-6ED879B71F83",
+   "name": "Mobile App App Flow"
+  }
+ ],
+ "startPageIndex": 0,
+ "totalImages": 18
+}
